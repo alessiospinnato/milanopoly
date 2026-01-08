17d940da7e49b7aa9d50f8339e8521e123ce9bc8
alessio.spinnato <spinna.jd@gmail.com>
2026-01-08 14:30:47 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Sostituisci 'milanopoly' con il nome ESATTO del tuo repository
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/alessiospinnato/milanopoly/', 
+  base: '/milanopoly/', // ✅ Corretto (solo il nome tra slash)
 })
